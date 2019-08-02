53cd17ad9bfb7991197e239361041891556656fc
Pierce McGeough <pmcgeough@hotmail.co.uk>
2019-08-03 00:23:10 +0100

    Convert to jigsaw

---
+++ source/index.blade.php
@@ -0,0 +1,12 @@
+@extends('_layouts.master')
+
+@section('body')
+    <div id="coming-soon">
+        <h1>Coming Sooner or Later</h1>
+
+        <p>Coming soon for all your website development and consultancy needs.</p>
+        <p>Consultancy and Development to help get your website or online product up and running.</p>
+
+        <a href="https://twitter.com/PierceMcGeough" title="Visit Pierce McGeough on Twitter" class="center">@PierceMcGeough</a>
+    </div>
+@endsection
